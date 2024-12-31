c244c913daadf476a4456699e653f877a6c9975e
Henry-ai443 <henrymaina2024@outlook.com>
2024-12-31 05:16:39 -0500

    Arrays in Javascript

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// *******ARRAYS********
+
+//1.ARRAY LITERAL
+const colors = ['red', 'green', 'yellow', 'blue']
+const numbers = [1, 2, 3, 4, 5]
+const mixed = [10, 'hello', true, null]
+
+//2.ARRAY CONSTRUCTOR
+const fruits = new Array("apple", "banana", "orange");
